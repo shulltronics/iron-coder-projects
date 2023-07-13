c2b758f8ba433efd4d1264dc22c45dd89f5f425f
Carsten Thue-Bludworth <carstentb@protonmail.com>
2023-07-13 22:28:49 +0900

    projects as they exist on my Arch Linux machine

--- my_cool_project/src/sys_mod_output_testing.rs
+++ my_cool_project/src/sys_mod_output_testing.rs
@@ -1,10 +1,8 @@
 use iron_coder_feather_rp2040_bsp;
 use iron_coder_featherwing_propmaker_bsp;
 pub struct System {
-    pub feather_rp2040: iron_coder_feather_rp2040_bsp::Board,
-    pub propmaker_featherwing: iron_coder_featherwing_propmaker_bsp::Board<
-        iron_coder_feather_rp2040_bsp::I2CBus,
-    >,
+    feather_rp2040: iron_coder_feather_rp2040_bsp::Board,
+    propmaker_featherwing: Board<iron_coder_feather_rp2040_bsp::I2CBus>,
 }
 impl System {
     pub fn new() -> Self {
