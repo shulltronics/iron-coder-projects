608e41cf9f22bbc05fdeca597434032af149f08f
Carsten Thue-Bludworth <carstentb@protonmail.com>
2023-07-20 23:51:28 +0900

    updating submodule

--- spi-disaply-testing/src/main.rs
+++ spi-disaply-testing/src/main.rs
@@ -14,7 +14,8 @@ use embedded_graphics::{
 	mono_font::{ascii::FONT_6X10, MonoTextStyle},
 	pixelcolor::Rgb565,
 };
-//use embedded_graphics_core::draw_target::DrawTarget;
+
+use smart_leds::RGB8;
 
 #[entry]
 fn main() -> ! {
@@ -25,6 +26,20 @@ fn main() -> ! {
 	system.main_board.set_led(true);
 
 	system.init_display();
+	//system.init_touchscreen();
+	system.main_board.delay(1000);
+
+	let i2c = system.main_board.i2c_manager.acquire_i2c();
+	let mut ts = ft6236::FT6236::new(i2c);
+	let n = match ts.get_number_of_touches() {
+		Ok(n) => n,//system.main_board.serial_write(n),
+		Err(e) => 100,//system.main_board.serial_write(e),
+	};
+
+	system.main_board.serial_write(n);
+	//system.display.init_touchscreen(i2c);
+	// system.clear_display();
+    system.display.display.as_mut().unwrap().clear(Rgb565::new(0, 0, 0));
 
 	// Create a new character style
     let style = MonoTextStyle::new(&FONT_6X10, Rgb565::RED);
@@ -32,19 +47,30 @@ fn main() -> ! {
     // Create a text at position (20, 30) and draw it using the previously defined style
     Text::with_alignment(
         "First line\nSecond line",
-        Point::new(20, 30),
+        Point::new(100, 30),
         style,
         Alignment::Center,
-    );
-    //.draw(&mut system.display.unwrap().0)
-    //.unwrap();
+    )
+    .draw(&mut system.display.display.unwrap())
+    .unwrap();
 
-	//system.display.unwrap().0.clear(Rgb565::new(255, 0, 0));
+	let colors: [RGB8; 3] = [
+		RGB8::new(255, 0, 0),
+		RGB8::new(0, 255, 0),
+		RGB8::new(0, 0, 255),
+	];
+
+	let mut color_iter = colors.iter().cycle();
+	//let mut ts = system.display.touchscreen.take().unwrap();
 
     loop {
+		system.main_board.set_neopixel_color(*color_iter.next().unwrap());
         // loop code goes here!
-
-
+		system.main_board.set_led(true);
+		system.main_board.delay(500);
+		system.main_board.set_led(false);
+		system.main_board.delay(500);
+		system.main_board.serial_write(n);
     }
 
 }
--- spi-disaply-testing/src/system.rs
+++ spi-disaply-testing/src/system.rs
@@ -3,31 +3,38 @@ use iron_coder_pitft_32_cap_touch_bsp;
 
 use embedded_graphics::pixelcolor::Rgb565;
 //use embedded_graphics::prelude::*;
-use embedded_graphics_core::draw_target::DrawTarget;
+use embedded_graphics::draw_target::DrawTarget;
 
-pub struct System {
+type I2CType<'a> = shared_bus::I2cProxy<'a, shared_bus::NullMutex<iron_coder_feather_rp2040_bsp::I2CBus>>;
+
+pub struct System<'a> {
 	pub main_board: iron_coder_feather_rp2040_bsp::Board,
-	pub display: Option<iron_coder_pitft_32_cap_touch_bsp::Display<iron_coder_feather_rp2040_bsp::SPIBus, iron_coder_feather_rp2040_bsp::Cs, iron_coder_feather_rp2040_bsp::Dc, iron_coder_feather_rp2040_bsp::Rst>>,
+	pub display: iron_coder_pitft_32_cap_touch_bsp::Display<iron_coder_feather_rp2040_bsp::SPIBus, iron_coder_feather_rp2040_bsp::Dc, iron_coder_feather_rp2040_bsp::Cs, iron_coder_feather_rp2040_bsp::Rst, I2CType<'a>>,
 }
-impl System {
+impl<'a> System<'a> {
     pub fn new() -> Self {
         Self {
 			main_board: iron_coder_feather_rp2040_bsp::Board::new(),
-			display: None,
+			display:  iron_coder_pitft_32_cap_touch_bsp::Display::new(),
 		}
     }
 
 	pub fn init_display(&mut self) {
-		let mut delay = self.main_board.delay_timer.take().unwrap();
+		//let mut delay = self.main_board.delay_timer.take().unwrap();
 		let spi = self.main_board.spi_bus.take().unwrap();
 		let cs = self.main_board.d4.take().unwrap();
-		let dc = self.main_board.d24.take().unwrap();
-		let rst = self.main_board.d25.take().unwrap();
-		self.display = Some(iron_coder_pitft_32_cap_touch_bsp::Display::init(spi, cs, dc, rst, delay));
+		let dc = self.main_board.d25.take().unwrap();
+		let rst = self.main_board.d5.take().unwrap();
+		self.display.init_display(spi, dc, cs, rst, &mut self.main_board.delay_timer);
+	}
+
+	pub fn init_touchscreen(&'a mut self) {
+		let i2c = self.main_board.i2c_manager.acquire_i2c();
+		self.display.init_touchscreen(i2c);
 	}
 
 	pub fn clear_display(&mut self) {
-		self.display.unwrap().0.clear(Rgb565::new(0, 0, 0));
+		self.display.display.as_mut().unwrap().clear(Rgb565::new(0, 0, 0));
 	}
 
 }
